7afb422dc2366e5795c66e6fb06e9a87069d2095
raifreire <rai.freire26@gmail.com>
2022-10-06 17:44:30 -0300

    add_Modal_In_Headers

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
 import { Container, Content } from './style'
 import logoImg from '../../assets/logo.svg'
+import { useState } from 'react'
+import Modal from 'react-modal'
 
 
 export function Header() {
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+    function handleCloseTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
 
         <Container>
             <Content>
                 <img src={logoImg} alt="finance control" />
-                <button type='button'>Nova transação</button>
+                <button type='button' onClick={handleOpenTransactionModal}>Nova transação</button>
+
+                <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal}>
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </Content>
 
         </Container>
