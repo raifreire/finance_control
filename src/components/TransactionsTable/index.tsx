29019a44b417dffc35fe4d8fce6d4ec9af3889f4
raifreire <rai.freire26@gmail.com>
2022-10-05 23:10:28 -0300

    add_transaction

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./style";
+
+
+export function TransactionTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de websites</td>
+                        <td className="deposit">120000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/09/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Alugel</td>
+                        <td className="withdraw">-120</td>
+                        <td>casa</td>
+                        <td>20/09/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
